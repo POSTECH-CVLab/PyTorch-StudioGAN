397db3a4cab3269ac8b05d72eab3765ae58348d9
MingukKANG <mgkang@postech.ac.kr>
2020-06-19 16:06:11 +0900

    Update prepare_inception_moments_eval_dataset.py

--- metrics/prepare_inception_moments_eval_dataset.py
+++ metrics/prepare_inception_moments_eval_dataset.py
@@ -1,4 +1,4 @@
- # PyTorch GAN Shop: https://github.com/POSTECH-CVLab/PyTorch-GAN-Shop
+# PyTorch GAN Shop: https://github.com/POSTECH-CVLab/PyTorch-GAN-Shop
 # The MIT License (MIT)
 # See license file or visit https://github.com/POSTECH-CVLab/PyTorch-GAN-Shop for details
 
