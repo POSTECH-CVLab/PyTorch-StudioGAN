0fcdee60960bc27a9d2af8560e2a6d3a86a693ce
MingukKANG <mgkang@postech.ac.kr>
2020-06-19 15:52:25 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+ # PyTorch GAN Shop: https://github.com/POSTECH-CVLab/PyTorch-GAN-Shop
+# The MIT License (MIT)
+# See license file or visit https://github.com/POSTECH-CVLab/PyTorch-GAN-Shop for details
+
+# main.py
+
+
 from argparse import ArgumentParser
 import json
 import os
