7f756c353485f7009955c8677620a3060729b8c5
MINGUKKANG <first287@naver.com>
2021-02-22 19:57:07 +0000

    add ddp script

--- ddp_script.sh
+++ ddp_script.sh
@@ -1,7 +1,7 @@
 export CUDA_VISIBLE_DEVICES=0,...,N_GPUS
-export NCCL_DEBUG=INFO
+# export NCCL_DEBUG=INFO
 export NCCL_SOCKET_IFNAME=^docker0,lo
-export MASTER_ADDR="MASTER_IP"
-export MASTER_PORT="MASTER_PORT"
+export MASTER_ADDR=MASTER_IP
+export MASTER_PORT=MASTER_PORT
 
 python3 src/main.py -t -e -rm_API -c CONFIG_PATH -DDP -n NUM_NODES -nr CURRENT_NODES -eval_type EVAL_TYPE ...
