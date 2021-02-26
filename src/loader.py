9148fd71411115b8d259d78ce06398156aecd25e
MINGUKKANG <first287@naver.com>
2021-02-26 10:42:08 +0000

    hotfix ema module for SyncBN

--- src/loader.py
+++ src/loader.py
@@ -18,7 +18,7 @@ from utils.log import make_checkpoint_dir, make_logger
 from utils.losses import *
 from utils.load_checkpoint import load_checkpoint
 from utils.misc import *
-from utils.biggan_utils import ema
+from utils.biggan_utils import ema, ema_DP_SyncBN
 from sync_batchnorm.batchnorm import convert_model
 from worker import make_worker
 
@@ -100,7 +100,10 @@ def prepare_train_eval(local_rank, gpus_per_node, world_size, run_name, train_co
         Gen_copy = module.Generator(cfgs.z_dim, cfgs.shared_dim, cfgs.img_size, cfgs.g_conv_dim, cfgs.g_spectral_norm, cfgs.attention,
                                     cfgs.attention_after_nth_gen_block, cfgs.activation_fn, cfgs.conditional_strategy, cfgs.num_classes,
                                     initialize=False, G_depth=cfgs.G_depth, mixed_precision=cfgs.mixed_precision).to(local_rank)
-        Gen_ema = ema(Gen, Gen_copy, cfgs.ema_decay, cfgs.ema_start)
+        if cfgs.distributed_data_parallel and cfgs.synchronized_bn:
+            Gen_ema = ema_DP_SyncBN(Gen, Gen_copy, cfgs.ema_decay, cfgs.ema_start)
+        else:
+            Gen_ema = ema(Gen, Gen_copy, cfgs.ema_decay, cfgs.ema_start)
     else:
         Gen_copy, Gen_ema = None, None
 
--- src/utils/biggan_utils.py
+++ src/utils/biggan_utils.py
@@ -47,18 +47,45 @@ class ema(object):
     with torch.no_grad():
       for key in self.source_dict:
         self.target_dict[key].data.copy_(self.source_dict[key].data)
-        # target_dict[key].data = source_dict[key].data # Doesn't work!
+        self.target_dict[key].requires_grad = False
+
 
   def update(self, itr=None):
     # If an iteration counter is provided and itr is less than the start itr,
     # peg the ema weights to the underlying weights.
-    if itr and itr < self.start_itr:
+    if itr >= 0 and itr < self.start_itr:
       decay = 0.0
     else:
       decay = self.decay
     with torch.no_grad():
       for key in self.source_dict:
         self.target_dict[key].data.copy_(self.target_dict[key].data * decay + self.source_dict[key].data * (1 - decay))
+
+
+class ema_DP_SyncBN(object):
+  def __init__(self, source, target, decay=0.9999, start_itr=0):
+    self.source = source
+    self.target = target
+    self.decay = decay
+    self.start_itr = start_itr
+    # Initialize target's params to be source's
+    print('Initializing EMA parameters to be source parameters...')
+    for key in self.source.state_dict():
+        self.target.state_dict()[key].data.copy_(self.source.state_dict()[key].data)
+        self.target.state_dict()[key].requires_grad = False
+
+
+  def update(self, itr=None):
+    # If an iteration counter is provided and itr is less than the start itr,
+    # peg the ema weights to the underlying weights.
+    if itr >= 0 and itr < self.start_itr:
+      decay = 0.0
+    else:
+      decay = self.decay
+
+    for key in self.source.state_dict():
+        data = self.target.state_dict()[key].data*decay + self.source.state_dict()[key].detach().data*(1.-decay)
+        self.target.state_dict()[key].data.copy_(data)
 
 
 def ortho(model, strength=1e-4, blacklist=[]):
