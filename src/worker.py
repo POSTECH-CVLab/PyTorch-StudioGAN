f1f1c22dfaca535cfb281f53a2c5292a6f78d24b
MINGUKKANG <first287@naver.com>
2021-09-19 14:34:40 +0900

    debug worker.py for CIFAR100

--- src/worker.py
+++ src/worker.py
@@ -126,12 +126,14 @@ class WORKER(object):
         if self.RUN.mixed_precision:
             self.scaler = torch.cuda.amp.GradScaler()
 
-        if self.DATA.name == "ImageNet":
-            self.num_eval = {"train": 50000, "valid": 50000}
-        elif self.DATA.name == "Tiny_ImageNet":
-            self.num_eval = {"train": 50000, "valid": 10000}
         elif self.DATA.name == "CIFAR10":
             self.num_eval = {"train": 50000, "test": 10000}
+        elif self.DATA.name == "CIFAR100":
+            self.num_eval = {"train": 50000, "test": 10000}
+        elif self.DATA.name == "Tiny_ImageNet":
+            self.num_eval = {"train": 50000, "valid": 10000}
+        elif self.DATA.name == "ImageNet":
+            self.num_eval = {"train": 50000, "valid": 50000}
         else:
             self.num_eval = {"train": len(self.train_dataloader.dataset), "valid": len(self.eval_dataset.dataset)}
 
