fc653bc11041d86165607e531a2c0a94b9be8eb8
MingukKANG <mgkang@postech.ac.kr>
2021-11-02 05:14:08 +0900

    Delete run_imagenet.sh

--- scripts/run_imagenet.sh
+++
@@ -1,3 +0,0 @@
-# export MASTER_ADDR="localhost"
-# export MASTER_PORT=8328
-CUDA_VISIBLE_DEVICES=0,1,2,3 python3 src/main.py -t -e -l -hdf5 -cfg "./src/configs/ImageNet/BigGAN-Mod256.yaml" -data "/home/minguk/studiogan/data/ImageNet" -save "/home/minguk/studiogan" -sync_bn -ref "valid" --entity "minguk" --project "StudioGAN"
