1440fc8af7480be6f219e23676869ab6061271b2
MingukKANG <mgkang@postech.ac.kr>
2021-11-02 05:13:59 +0900

    Delete run_cifar10.sh

--- scripts/run_cifar10.sh
+++
@@ -1,3 +0,0 @@
-# export MASTER_ADDR="localhost"
-# export MASTER_PORT=8328
-CUDA_VISIBLE_DEVICES=0 python3 src/main.py -t -e -l -hdf5 -cfg "./src/configs/CIFAR10/BigGAN-Mod.yaml" -data "/home/minguk/studiogan/data/CIFAR10" -save "/home/minguk/studiogan" -batch_stat -ref "test" --entity "minguk" --project "StudioGAN"
