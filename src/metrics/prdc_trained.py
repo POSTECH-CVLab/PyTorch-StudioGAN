9031e17ebe0cdd3f97d9ee8610af04c6f0fb2bac
MingukKANG <mgkang@postech.ac.kr>
2021-11-02 05:38:54 +0900

    Update prdc_trained.py

--- src/metrics/prdc_trained.py
+++ src/metrics/prdc_trained.py
@@ -1,25 +1,8 @@
-# coding=utf-8
-# Taken from:
-# https://github.com/google/compare_gan/blob/master/compare_gan/src/prd_score.py
-#
-# Changes:
-#   - default dpi changed from 150 to 300
-#   - added handling of cases where P = Q, where precision/recall may be
-#     just above 1, leading to errors for the f_beta computation
-#
-# Copyright 2018 Google LLC & Hwalsuk Lee.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
+# PyTorch StudioGAN: https://github.com/POSTECH-CVLab/PyTorch-StudioGAN
+# The MIT License (MIT)
+# See license file or visit https://github.com/POSTECH-CVLab/PyTorch-StudioGAN for details
+
+# src/metrics/prdc_trained.py
 
 from tqdm import tqdm
 import math
