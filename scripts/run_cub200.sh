0b3949f2bde55863ab279af3aac771ce9c91de49
MingukKANG <mgkang@postech.ac.kr>
2021-11-02 05:14:04 +0900

    Delete run_cub200.sh

--- scripts/run_cub200.sh
+++
@@ -1,3 +0,0 @@
-# export MASTER_ADDR="localhost"
-# export MASTER_PORT=8328
-CUDA_VISIBLE_DEVICES=0,1,2,3 python3 src/main.py -t -e -l -hdf5 -cfg "./src/configs/CUB200/BigGAN-Mod.yaml" -data "/home/minguk/studiogan/data/CUB200" -save "/home/minguk/studiogan" -sync_bn -ref "train" --entity "minguk" --project "StudioGAN"
